c8437f71a3ad05a3ba5bc365d32d1c2e72a3bde6
pgulati14 <84927488+pgulati14@users.noreply.github.com>
2021-09-28 18:40:42 +0530

    Add files via upload

--- lookup.rb
+++ lookup.rb
@@ -9,32 +9,49 @@ def get_command_line_argument
   end
   ARGV.first
 end
+
 # `domain` contains the domain name we have to look up.
 domain = get_command_line_argument
+
 # File.readlines reads a file and returns an
 # array of string, where each element is a line
 # https://www.rubydoc.info/stdlib/core/IO:readlines
 dns_raw = File.readlines("zone")
+# FILL YOUR CODE HERE.
+def parse_dns(dns_raw)
+  dns_raw.reject { |x| (x[0] == "#") || (x[0] == "\n") }.
+    map { |x| x.strip().split(", ") }.
+    each_with_object({}) do |record, records|
+    records[record[1]] = { type: record[0], target: record[2] }
+  end
 
-def parse_dns(init)
-  init = init.reject { |line| line.start_with?(/^$/) }
-  init = init.reject { |line| line.start_with?(/^#/) }
-  arr = Array.new
-  arr = init.map { |line| line.strip.split(",") }
-  hash = Hash.new
-  hash = arr.each_with_object({}) {|r, i| i[r[1].strip] = { type: r[0].strip, target: r[2].strip}}
-  return hash
 end
+#def parse_dns(raw)
+ # init=raw.
+  #reject {|line| line.empty? }.
+  #map {|line| line.strip.split(", ") }
+  #init = init.map(&:strip)
+  #init = init.reject { |line| line.empty? }
+  #init = init.reject { |line| line.start_with?("#") }
+  #init = init.reject { |line| line.start_with?("$") }
+  #arr = init.map { |line| line.strip.split(", ") }
+  #each_with_object({}) do |record, records|
+   # records[record[1]] = { type: record[0], target: record[2] } end
+  #arr = init.reject { |record| record.length!= 3 }
+  #dns_records = arr.each_with_object({}) do |record, records| records[record[1]] =
+   # { type: record.first, target: record.last } end
+   # dns_records
 
 def resolve(dns_records, lookup_chain, domain)
-  rec = dns_records.fetch(domain, nil)
-  if rec == nil
+  record = dns_records[domain]
+  #rec = dns_records.fetch(domain, nil)
+  if (!record)
     lookup_chain << "Error: record not found for #{domain}"
-  elsif rec[:type] == "A"
-    lookup_chain << rec[:target]
-  elsif rec[:type] == "CNAME"
-    lookup_chain << rec[:target]
-    resolve(dns_records, lookup_chain, rec[:target])
+  elsif record[:type] == "A"
+    lookup_chain << record[:target]
+  elsif record[:type] == "CNAME"
+    lookup_chain << record[:target]
+    resolve(dns_records, lookup_chain, record[:target])
   end
 end
 # To complete the assignment, implement `parse_dns` and `resolve`.
