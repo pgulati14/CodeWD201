2e704e56fe8547420ae4364f7b4a67da68d0d916
pgulati14 <84927488+pgulati14@users.noreply.github.com>
2021-10-01 00:55:40 +0530

    Add files via upload

---
+++ Level 4/todo.rb
@@ -0,0 +1,54 @@
+require "active_record"
+require "./connect_db.rb"
+
+class Todo < ActiveRecord::Base
+  def due_today?
+    due_date == Date.today
+  end
+
+  def self.overdue
+    where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    where(due_date: Date.today)
+  end
+
+  def self.due_later
+    where("due_date > ?", Date.today)
+  end
+
+  def to_displayable_string
+    display_status = completed ? "[X]" : "[ ]"
+    display_date = due_today? ? nil : due_date
+    "#{id}. #{display_status} #{todo_text} #{display_date}"
+  end
+
+  def self.show_list
+    puts "My Todo-list\n\n"
+
+    puts "Overdue\n"
+    puts overdue.map { |todo| todo.to_displayable_string }
+    puts "\n"
+
+    puts "Due Today\n"
+    puts due_today.map { |todo| todo.to_displayable_string }
+    puts "\n"
+
+    puts "Due Later\n"
+    puts due_later.map { |todo| todo.to_displayable_string }
+    puts "\n"
+  end
+   def self.add_task(todo)
+    todo_text = todo[:todo_text]
+    due_date = Date.today + todo[:due_in_days]
+    create!(todo_text: todo_text, due_date: due_date, completed: false)
+  end
+
+  def self.mark_as_complete(todo_id)
+    todo_completion = find(todo_id)
+    todo_completion.completed = true
+    todo_completion.save
+    return todo_completion
+  end
+end
